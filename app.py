62d388b22be4ffb3c86d607ce21ef99ab87f3c95
Mourdhwaj <83101773+Mourdhwaj@users.noreply.github.com>
2024-09-15 20:16:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+import subprocess
+import sys
+
+def install_requirements():
+    try:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while installing packages: {e}")
+        sys.exit(1)
+
+# Call the function to install requirements
+install_requirements()
+
 import streamlit as st
 import numpy as np 
 import tensorflow as tf
